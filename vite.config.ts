f2dc2793afe580eb7cde3ddb5c195abeed790b5f
Luke Hagar <lukeslakemail@gmail.com>
2024-05-17 11:39:08 -0500

    removed sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
 	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'sentry',
-				project: 'oas-def-gen',
-				url: 'https://sentry.plygrnd.org',
-				authToken: process.env.SENTRY_AUTH_TOKEN
-			}
-		}),
 		sveltekit(),
 		purgeCss({
 			safelist: {
